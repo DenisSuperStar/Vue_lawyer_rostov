a4f9dccd6607eb560038b23ee80e611257f7754b
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-07-23 18:36:52 +0300

    Изменен дизайн секции прайс-лист

--- src/components/PriceList.vue
+++ src/components/PriceList.vue
@@ -1,220 +1,302 @@
 <template>
-    <fragment>
-        <div :class="[$style.pricesContainer]">
-            <h3 :class="[$style.pricesTitle]">02__Прайс лист</h3>
-            <VueSlickCarousel :arrows="false" :dots="false" :touchMove="true" :slidesToShow="3" :slidesToScroll="3"
-                :speed="500">
-                <div :class="[$style.priceItem]" v-for="price in priceList" :key="price.id">
-                    <div :class="[$style.cardItem]">
-                        <div :class="[$style.cardHeader]">
-                            <div :class="[$style.cardTitle]">{{ price.title }}</div>
-                            <div :class="[$style.cardSubtitle]">{{ price.subtitle }}</div>
-                            <div :class="[$style.cardPrice]">{{ price.price }}</div>
-                        </div>
-                        <div :class="[$style.cardBody]">
-                            <ul :class="[$style.cardFeatures]">
-                                <li :class="[$style.featuresItem]">{{ price.text }}</li>
-                            </ul>
-                        </div>
-                        <a href="#" :class="[$style.pricingButton]">Выбрать</a>
-                    </div>
+<div :class="[$style.priceListContainer]">
+    <div :class="[$style.priceListCentered]">
+      <div :class="[$style.filledIconReverse]">
+        <font-awesome-icon icon="fa-solid fa-briefcase" />
+      </div>
+      <h3 :class="[$style.priceListTitle]">02__Прайс лист</h3>
+      <VueSlickCarousel
+        :arrows="false"
+        :dots="false"
+        :touchMove="true"
+        :slidesToShow="3"
+        :slidesToScroll="3"
+        :speed="500"
+      >
+        <div
+          :class="[$style.priceListItem]"
+          v-for="price in priceList"
+          :key="price.id"
+        >
+          <div :class="$style.infoCard">
+            <div :class="[$style.infoHeader]">
+              <div :class="[$style.columnLeft]">
+                <div :class="[$style.infoTitle]">{{ price.title }}</div>
+                <div :class="[$style.infoSubtitle]">{{ price.subtitle }}</div>
+              </div>
+              <div :class="[$style.columnRight]">
+                <div :class="[$style.contentInfo]">{{ price.price }}</div>
+              </div>
+            </div>
+
+            <ul :class="[$style.cardFeatures]">
+              <li :class="[$style.featuresItem]">
+                <div class="[$style.featuresCheckbox]">
+                  <input
+                    type="checkbox"
+                    :class="[$style.defaultCheckbox]"
+                    checked
+                  />
+                  <span :class="[$style.fakerCheckbox]"></span>
                 </div>
-            </VueSlickCarousel>
+                <div :class="[$style.featuresText]">По телефону</div>
+              </li>
+              <li :class="[$style.featuresItem]">
+                <div class="[$style.featuresCheckbox]">
+                  <input
+                    type="checkbox"
+                    :class="[$style.defaultCheckbox]"
+                    checked
+                  />
+                  <span :class="[$style.fakerCheckbox]"></span>
+                </div>
+                <span :class="[$style.featuresText]"
+                  >Вы оставляете заявку на сайте</span
+                >
+              </li>
+              <li :class="[$style.featuresItem]">
+                <div class="[$style.featuresCheckbox]">
+                  <input
+                    type="checkbox"
+                    :class="[$style.defaultCheckbox]"
+                    checked
+                  />
+                  <span :class="[$style.fakerCheckbox]"></span>
+                </div>
+                <span :class="[$style.featuresText]"
+                  >Менеджер связывается с вами для обсуждения деталей</span
+                >
+              </li>
+            </ul>
+
+            <a href="#" :class="[$style.pricingButton]">Выбрать</a>
+          </div>
         </div>
-    </fragment>
+      </VueSlickCarousel>
+    </div>
+    </div>
 </template>
 
 <script>
-import { Fragment } from 'vue-fragment';
-import VueSlickCarousel from 'vue-slick-carousel';
-import 'vue-slick-carousel/dist/vue-slick-carousel.css';
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default {
-    name: 'PriceList',
-    components: {
-        Fragment,
-        VueSlickCarousel
-    },
-    data: function () {
-        return {
-            priceList: [
-                {
-                    id: 1,
-                    title: 'Устная консультация для граждан',
-                    subtitle: 'Выбор пользователей',
-                    price: 'Бесплатно',
-                    text: 'По телефону'
-                },
-                {
-                    id: 2,
-                    title: 'Устная консультация для граждан с изучением документов',
-                    subtitle: 'Оптимальный выбор',
-                    price: '1000руб',
-                    text: 'Стоимость расчитывается из проведенной консультации'
-                },
-                {
-                    id: 3,
-                    title: 'Письменная консультация для граждан',
-                    subtitle: 'Оптимальный выбор',
-                    price: '2000руб',
-                    text: 'Стоимость за один документ'
-                },
-                {
-                    id: 4,
-                    title: 'Составление претензии',
-                    subtitle: 'Выбор пользователей',
-                    price: '1000руб',
-                    text: 'Стоимость за один документ'
-                },
-                {
-                    id: 5,
-                    title: 'Составление гражданско-правовых, трудовых договоров',
-                    subtitle: 'Выбор пользователей',
-                    price: '2000руб',
-                    text: 'Срок исполнения от 1 до 3 дней'
-                },
-                {
-                    id: 6,
-                    title: 'Составление сложных (смешанные формы), нестандартных договоров',
-                    subtitle: 'Выбор пользователей',
-                    price: '3000руб',
-                    text: 'Срок исполнения от 3 до 5 дней'
-                },
-                {
-                    id: 7,
-                    title: 'Составление процессуальных документов (иск, отзыв на иск и т.п.)',
-                    subtitle: 'Выбор пользователей',
-                    price: '3000руб',
-                    text: 'Стоимость за один документ'
-                }
-            ]
-        }
-    }
-}
+  name: "PriceList",
+  components: {
+    VueSlickCarousel,
+  },
+  data: function () {
+    return {
+      priceList: [
+        {
+          id: 1,
+          title: "Устная консультация для граждан",
+          subtitle: "Выбор пользователей",
+          price: "Бесплатно",
+          text: "По телефону",
+        },
+        {
+          id: 2,
+          title: "Устная консультация для граждан с изучением документов",
+          subtitle: "Оптимальный выбор",
+          price: "1000руб",
+          text: "Стоимость расчитывается из проведенной консультации",
+        },
+        {
+          id: 3,
+          title: "Письменная консультация для граждан",
+          subtitle: "Оптимальный выбор",
+          price: "2000руб",
+          text: "Стоимость за один документ",
+        },
+        {
+          id: 4,
+          title: "Составление претензии",
+          subtitle: "Выбор пользователей",
+          price: "1000руб",
+          text: "Стоимость за один документ",
+        },
+        {
+          id: 5,
+          title: "Составление гражданско-правовых, трудовых договоров",
+          subtitle: "Выбор пользователей",
+          price: "2000руб",
+          text: "Срок исполнения от 1 до 3 дней",
+        },
+        {
+          id: 6,
+          title:
+            "Составление сложных (смешанные формы), нестандартных договоров",
+          subtitle: "Выбор пользователей",
+          price: "3000руб",
+          text: "Срок исполнения от 3 до 5 дней",
+        },
+        {
+          id: 7,
+          title:
+            "Составление процессуальных документов (иск, отзыв на иск и т.п.)",
+          subtitle: "Выбор пользователей",
+          price: "3000руб",
+          text: "Стоимость за один документ",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style module>
-.pricesContainer {
+.priceListContainer {
+    background-color: #6630ff;
+}
+
+.priceListCentered {
     max-width: 1240px;
     margin: 0 auto;
-    padding-top: 58.5px;
 }
 
-.pricesTitle {
-    font-size: 16px;
-    line-height: 1;
-    text-transform: uppercase;
-    color: #6630FF;
-    padding: 0 13px 6.5px;
-}
-
-.priceList {
+.filledIconReverse {
+    width: 120px;
+    height: 120px;
     display: flex;
-    width: 100%;
-    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    color: #6630ff;
+    font-size: 60px;
+    line-height: 1.1;
+    padding: 6.5px 13px;
 }
 
-.priceItem {
-    padding: 0 13px;
+.priceListTitle {
+    font-size: 16px;
+    line-height: 1.1;
+    text-transform: uppercase;
+    color: #fff;
+    padding: 130px 13px 26px;
 }
 
-.cardItem {
+.priceListItem {
+  padding: 0 13px;
+}
+
+.infoCard {
     padding: 13px;
-    background-color: #F8EEFD;
+    border: 2px solid #fff;
+    background-color: #6630ff;
     cursor: pointer;
 }
 
-.cardHeader {
-    position: relative;
+.infoHeader {
+  display: flex;
+  position: relative;
 }
 
-.cardTitle {
-    max-width: 60%;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    font-size: 18px;
-    line-height: 1;
-    color: #111;
+.columnLeft {
+  flex: 7;
 }
 
-
-.cardSubtitle {
-    font-size: 12px;
-    line-height: 1;
-    color: #6630FF;
-    padding-bottom: 19.5px;
+.columnRight {
+  flex: 3;
 }
 
-.cardPrice {
-    position: absolute;
-    top: 0;
-    right: 0;
-    padding: 6.5px 13px;
+.infoTitle {
+    font-size: 16px;
+    line-height: 1.1;
     color: #fff;
-    font-size: 18px;
-    line-height: 1;
-    background: #6630FF;
+    padding-bottom: 6.5px;
 }
 
-.cardBody {
-    max-width: 80%;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
+.infoSubtitle {
+  font-size: 12px;
+  line-height: 1.1;
+  color: #6630ff;;
+  padding-bottom: 19.5px;
+}
+
+.contentInfo {
+    background-color: #fff;
+    color: #6630ff;
+    font-size: 16px;
+    line-height: 1.1;
+    padding: 6.5px 13px;
 }
 
 .cardFeatures {
-    padding-bottom: 26px;
+  list-style: none;
+  padding-bottom: 26px;
 }
 
 .featuresItem {
-    font-size: 16px;
-    line-height: 1;
+  display: flex;
+  grid-gap: 1.3px;
+  font-size: 16px;
+  line-height: 1.1;
+  padding-bottom: 6.5px;
+}
+
+.featuresIcon::before {
+  content: "";
+}
+
+.featuresCheckbox {
+}
+
+.defaultCheckbox {
+}
+
+.fakerCheckbox {
+}
+
+.featuresText {
+  color: #fff;
+  font-size: 16px;
+  line-height: 1.1;
 }
 
 .pricingButton {
-    width: 100%;
     display: block;
-    background-color: #6630FF;
-    color: #fff;
+    background-color: #fff;
+    color: #6630ff;
+    width: 100%;
     text-decoration: none;
     font-size: 16px;
-    line-height: 1;
+    line-height: 1.1;
     text-align: center;
     padding: 13px 26px;
 }
 
 .pricingButton::after {
-    content: none !important;
+  content: none !important;
 }
 </style>
 
 <style>
 .slick-slider {
-    height: auto;
+  height: auto;
 }
 
 .slick-dots li {
-    width: 10px;
-    height: 10px;
-    background-color: #F8EEFD;
-    cursor: pointer;
+  width: 10px;
+  height: 10px;
+  background-color: #f8eefd;
+  cursor: pointer;
 }
 
 .slick-dots li button {
-    width: 10px;
-    height: 10px;
+  width: 10px;
+  height: 10px;
 }
 
 .slick-dots li button::before {
-    content: "";
-    width: 10px;
-    height: 10px;
+  content: "";
+  width: 10px;
+  height: 10px;
 }
 
 .slick-dots .slick-active {
-   background-color: #6630FF;
+  background-color: #6630ff;
 }
 </style>
