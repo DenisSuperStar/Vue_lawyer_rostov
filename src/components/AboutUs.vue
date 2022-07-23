cf5236816ee39d66fe306a97c37bad7ee8c228fd
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-07-23 12:23:08 +0300

    Элементы слайдера преобразованы к похожей форме

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,187 +1,236 @@
 <template>
-    <fragment>
-        <div :class="[$style.sectionContent]">
-            <h3 :class="[$style.sectionHeadline]">01__О нас</h3>
-            <div :class="[$style.sectionSubline]">
-                <span :class="[$style.sublineInner]">Наша компания предоставляет консультации по полному спектру
-                    юридических услуг</span>
-            </div>
-            <ul :class="[$style.contentInner]">
-                <li :class="[$style.contentTextBox]" v-for="item in items" :key="item.id">
-                    <div :class="[$style.contentTitle]">{{ item.title }}</div>
-                    <div :class="[$style.contentText]">{{ item.text }}</div>
-                </li>
-            </ul>
+  <fragment>
+    <div :class="[$style.sectionContent]">
+      <div :class="[$style.sectionInner]">
+        <div :class="[$style.sectionInfo]">
+          <span :class="[$style.filledSectionIcon]">
+            <font-awesome-icon
+              :class="[$style.containerFillIcon]"
+              icon="fa-solid fa-question"
+            />
+          </span>
+          <h3 :class="[$style.sectionHeadline]">01__О нас</h3>
+          <div :class="[$style.sectionSubline]">
+            Наша компания предоставляет консультации по полному спектру
+            юридических услуг
+          </div>
         </div>
-        <VueSlickCarousel :arrows="false" :dots="false" :touchMove="true" :slidesToShow="2" :slidesToScroll="2"
-            :speed="500">
-            <div :class="[$style.cardsectionSlide]">
-                <div :class="[$style.cardsection, $style.cardsectionFirst]">
-                    <div :class="[$style.cardHeadersection]">
-                        <img src="../../public/assets/grid_1.svg" :class="[$style.cardAttacment]" alt="Развод" />
-                    </div>
-
-                    <div :class="[$style.cardBodyTitle]">Споры при разводе</div>
-                    <div :class="[$style.cardBodySubtitle]">Помощь в решении конфликтов при разводе родителей</div>
-
-                </div>
-            </div>
-            <div :class="[$style.cardsectionSlide]">
-                <div :class="[$style.cardsection]">
-                    <div :class="[$style.cardHeadersection]">
-                        <img src="../../public/assets/grid_2.svg" :class="[$style.cardAttacment]" alt="Алименты" />
-                    </div>
-
-                    <div :class="[$style.cardBodyTitle]">Алиментные споры</div>
-                    <div :class="[$style.cardBodySubtitle]">Урегулирование споров по взысканию алиментов</div>
-
-                </div>
-            </div>
-            <div :class="[$style.cardsectionSlide]">
-                <div :class="[$style.cardsection]">
-                    <div :class="[$style.cardHeadersection]">
-                        <img src="../../public/assets/grid_3.svg" :class="[$style.cardAttacment]" alt="Имущество" />
-                    </div>
-
-                    <div :class="[$style.cardBodyTitle]">Раздел имущества</div>
-                    <div :class="[$style.cardBodySubtitle]">Защита имущественных интересов клиента при разводе</div>
-
-                </div>
-            </div>
-            <div :class="[$style.cardsectionSlide]">
-                <div :class="[$style.cardsection]">
-                    <div :class="[$style.cardHeadersection]">
-                        <img src="../../public/assets/grid_4.svg" :class="[$style.cardAttacment]" alt="Усыновление" />
-                    </div>
-
-                    <div :class="[$style.cardBodyTitle]">Вопросы усыновления</div>
-                    <div :class="[$style.cardBodySubtitle]">Консультации при усыновлении детей</div>
-
-                </div>
-            </div>
-        </VueSlickCarousel>
-    </fragment>
+      </div>
+      <VueSlickCarousel
+        :arrows="false"
+        :dots="false"
+        :touchMove="true"
+        :slidesToShow="3"
+        :slidesToScroll="3"
+      >
+        <div
+          :class="[$style.carouselSlide]"
+          v-for="(post, index) in posts"
+          :key="index"
+        >
+          <div :class="[$style.filledCard]" v-if="post.id === 1">
+            <div :class="[$style.fillCardTitle]">{{ post.title }}</div>
+            <div :class="[$style.fillCardSubtitle]">{{ post.body }}</div>
+          </div>
+          <div :class="[$style.itemCard]" v-else>
+            <div :class="[$style.cardTitle]">{{ post.title }}</div>
+            <div :class="[$style.cardSubtitle]">{{ post.body }}</div>
+          </div>
+        </div>
+      </VueSlickCarousel>
+    </div>
+    <VueSlickCarousel
+      :arrows="false"
+      :dots="false"
+      :touchMove="true"
+      :slidesToShow="6"
+      :slidesToScroll="6"
+    >
+      <div
+        :class="[$style.carouselSlide]"
+        v-for="(solution, index) in solutions"
+        :key="index"
+      >
+        <div :class="[$style.filledCard]" v-if="solution.id === 1">
+          <img
+            :src="solution.avatar"
+            :class="[$style.cardImage]"
+            :alt="solution.first_name"
+          />
+          <div :class="[$style.fillCardTitle]">{{ solution.first_name }}</div>
+          <div :class="[$style.fillCardSubtitle]">
+            {{ solution.last_name }}: {{ solution.email }}
+          </div>
+        </div>
+        <div :class="[$style.itemCard]" v-else>
+          <img
+            :src="solution.avatar"
+            :class="[$style.cardImage]"
+            :alt="solution.first_name"
+          />
+          <div :class="[$style.cardTitle]">{{ solution.first_name }}</div>
+          <div :class="[$style.cardSubtitle]">{{ solution.last_name }}</div>
+        </div>
+      </div>
+    </VueSlickCarousel>
+  </fragment>
 </template>
 
 <script>
-import { Fragment } from 'vue-fragment';
-import VueSlickCarousel from 'vue-slick-carousel';
-import 'vue-slick-carousel/dist/vue-slick-carousel.css';
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import { Fragment } from "vue-fragment";
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import axios from "axios";
 
 export default {
-    name: "AboutUs",
-    components: {
-        Fragment,
-        VueSlickCarousel,
-    },
-    data: function () {
-        return {
-            items: [
-                {
-                    id: 1,
-                    title: 'Деятельность',
-                    text: 'Оперативное и профессиональное решение проблем по гражданским, семейным и жилищным трудовым делам.'
-                },
-                {
-                    id: 2,
-                    title: 'Цели',
-                    text: 'Своевременная помощь в решении ваших юридических проблем. Этапы помощи включают экспертизу, оценку и подготовку к консультации самого клиента. Выполняем поставленные задачи в срок в пределах вашего бюджета. Более 5 лет оказываем правовую помощь гражданам и организациям.'
-                }
-            ]
-        }
-    }
-}
+  name: "AboutUs",
+  components: {
+    Fragment,
+    VueSlickCarousel,
+  },
+  data: function () {
+    return {
+      posts: Array(25).fill({
+        title: "",
+        body: "",
+      }),
+      solutions: Array(12).fill({
+        avatar: null,
+        email: "",
+        first_name: "",
+        id: null,
+        last_name: "",
+      }),
+    };
+  },
+  created() {
+    axios.get("https://reqres.in/api/users?page=1&per_page=12").then((res) => {
+      const { data } = res.data;
+      this.solutions = data;
+    });
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts?_limit=25")
+      .then((res) => {
+        const { data } = res;
+        this.posts = data;
+      });
+  },
+};
 </script>
 
 <style module>
 .sectionContent {
-    background-color: #6630ff;
-    text-align: right;
-    padding: 20px 60px 40px 60px;
+  background-color: #6630ff;
+  text-align: right;
+  padding: 1px 60px 40px 60px;
+  margin-bottom: calc(52px - 13px);
+}
+
+.sectionInner {
+  display: flex;
+}
+
+.sectionInfo {
+  max-width: calc(50% - 9.75px);
+  margin-left: auto;
+}
+
+.filledSectionIcon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  height: 120px;
+  background-color: #f8eefd;
+    color: #6630ff;
+  font-size: 60px;
 }
 
 .sectionHeadline {
-    font-size: 16px;
-    text-transform: uppercase;
-    color: #fff;
-    padding-bottom: 6.5px;
+  font-size: 16px;
+  text-transform: uppercase;
+  color: #fff;
+  padding: 195px 0 6.5px;
 }
 
 .sectionSubline {
-    font-size: 27px;
-    line-height: 1;
-    color: #fff;
-    padding-bottom: 39px;
-}
-
-.sublineInner {
-    display: inline-block;
-    max-width: 45%;
-    margin: 0 auto;
-}
-
-.contentInner {
-    display: flex;
-    list-style-type: none;
-    grid-gap: 19.5px;
+  font-size: 35px;
+  line-height: 1.2;
+  color: #fff;
+  padding-bottom: 39px;
 }
 
 .contentTextBox {
-    width: 50%;
-    display: block;
-    background-color: #F8EEFD;
-    padding: 13px;
-    text-align: left;
+  width: 50%;
+  display: block;
+  background-color: #f8eefd;
+  padding: 13px;
+  text-align: left;
+  margin: 0 9.75px;
 }
 
 .contentTitle {
-    color: #111;
-    font-size: 27px;
-    line-height: 1.2;
-    margin-bottom: 6.5px;
+  color: #111;
+  font-size: 35px;
+  line-height: 1.2;
+  margin-bottom: 6.5px;
 }
 
 .contentText {
-    color: #111;
-    font-size: 16px;
-    line-height: 1;
-    text-align: justify;
+  color: #111;
+  font-size: 16px;
+  line-height: 1.2;
+  text-align: justify;
 }
 
-.cardsectionSlide {
-    padding: 0 13px;
+.carouselSlide {
+  padding: 0 13px;
 }
 
-.cardsection {
-    padding: 13px 13px 32.5px 13px;
-    cursor: pointer;
+.filledCard {
+  background-color: #f8eefd;
+  padding: 13px 13px 32.5px 13px;
 }
 
-.cardsectionFirst {
-    background-color: #F8EEFD;
+.itemCard {
+  padding: 13px 13px 32.5px 13px;
 }
 
-.cardHeadersection {
-    margin-bottom: 39px;
+.fillCardTitle {
+  font-size: 16px;
+  line-height: 1.1;
+  margin-bottom: 6.5px;
+  text-transform: uppercase;
+  color: #6630ff;
 }
 
-.cardBodyTitle {
-    font-size: 13px;
-    line-height: 1.8;
-    margin-bottom: 6.5px;
-    text-transform: uppercase;
-    color: #111;
+.cardTitle {
+  font-size: 16px;
+  line-height: 1.1;
+  margin-bottom: 6.5px;
+  text-transform: uppercase;
+  color: #111;
 }
 
-.cardBodySubtitle {
-    font-size: 13px;
-    line-height: 1.8;
-    color: #111;
+.fillCardSubtitle {
+  font-size: 16px;
+  line-height: 1.1;
+  color: #6630ff;
+  text-align: justify;
 }
 
-.cardAttacment {
-    display: block;
-    width: 100%;
+.cardSubtitle {
+  font-size: 16px;
+  line-height: 1.1;
+  color: #111;
+  text-align: justify;
+}
+
+.cardImage {
+  display: block;
+  width: 100%;
+  margin-bottom: 39px;
 }
 </style>
