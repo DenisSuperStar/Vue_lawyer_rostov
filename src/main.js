9c4120aef805b414ff5e5675ed7b31b9005e5256
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-07-23 18:35:46 +0300

    Импортированы другие иконки

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone, faQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faQuestion, faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faPhone);
 library.add(faQuestion);
+library.add(faBriefcase);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
