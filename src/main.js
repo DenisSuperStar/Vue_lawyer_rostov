86b222e4f9e3c84598801e50aa58e3c5dba72b95
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-07-27 01:16:56 +0300

    подключены иконки брендов

--- src/main.js
+++ src/main.js
@@ -2,13 +2,33 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone, faQuestion, faBriefcase, faFilePen } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPhone,
+  faQuestion,
+  faBriefcase,
+  faFilePen,
+  faClock,
+  faEnvelope,
+  faHouseChimney,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faVk,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPhone);
-library.add(faQuestion);
-library.add(faBriefcase);
-library.add(faFilePen);
+library.add(
+  faPhone,
+  faQuestion,
+  faBriefcase,
+  faFilePen,
+  faHouseChimney,
+  faClock,
+  faEnvelope
+);
+library.add(faTwitter, faVk, faInstagram, faLinkedinIn);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
