41e2b0ae399774ff1bba515301c6d88cc9d97701
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-08-08 03:06:21 +0300

    Создан компонент заголовок раздела с входными данными

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3 :class="[$style.sectionTitle]">{{name}}</h3>
+</template>
+
+<script>
+export default {
+  name: "SectionTitle",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style module>
+.sectionTitle {
+  line-height: 1.1;
+  text-transform: uppercase;
+  color: #fff;
+  padding: 260px 13px 26px;
+}
+
+@media all and (max-width: 768px) {
+  .sectionTitle {
+    padding-top: 204px;
+  }
+}
+</style>
