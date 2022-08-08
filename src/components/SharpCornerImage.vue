72abd83a104552aa5b663c5a000bc5f9e7027822
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-08-08 03:02:21 +0300

    Создан компонент изображение с острыми углами

---
+++ src/components/SharpCornerImage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div :class="[$style.imageWrapper]">
+    <div :class="[$style.borderlessWrapper]">
+      <img :src="founder" :class="[$style.photo]" alt="Основатель проекта" />
+    </div>
+  </div>
+</template>
+
+<script>
+import founderPhoto from "../../public/assets/leader.svg";
+
+export default {
+  name: "SharpCornerImage",
+  data: function () {
+    return {
+      founder: founderPhoto,
+    };
+  },
+};
+</script>
+
+<style module>
+.imageWrapper {
+  position: relative;
+}
+
+.imageWrapper::before,
+.imageWrapper::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border: 2px solid #fff;
+  z-index: 10;
+}
+
+.imageWrapper::before {
+  top: -13px;
+  left: -13px;
+}
+
+.imageWrapper::after {
+  right: -13px;
+  bottom: -13px;
+}
+
+.borderlessWrapper {
+  width: 100%;
+  position: relative;
+  background-color: #fff;
+  border: 2px solid #fff;
+  z-index: 100;
+}
+
+.photo {
+  display: block;
+  width: 100%;
+}
+</style>
