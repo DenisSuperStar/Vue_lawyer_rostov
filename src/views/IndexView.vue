bdb4337f3dcb6d3770737c9a032a2937b7f805d8
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-08-08 05:14:55 +0300

    Добавлены частичные представления

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,27 +1,12 @@
 <template>
   <div :class="[$style.app]">
-    <section :class="[$style.backgroundImgContainer]">
-      <app-header />
-      <section :class="[$style.carouselWrapper]">
-        <app-carousel :isDots="true" />
-      </section>
-    </section>
-    <section :class="[$style.aboutSection]">
-      <div :class="[$style.aboutWrapper]">
-        <div :class="[$style.aboutContent]">
-          <div :class="[$style.iconHolder, $style.iconHolder_1]">
-            <icon-filled kind="fa-solid fa-question" />
-          </div>
-          <section-title name="01__О нас" />
-          <div :class="[$style.appAboutUsSubline]">
-            Наша компания предоставляет консультации по полному спектру
-            юридических услуг
-          </div>
-        </div>
-      </div>
-      <app-touch-carousel :isShow="false" :slidesShow="3" :slidesScroll="3" />
-    </section>
-    <app-touch-carousel :isShow="true" :slidesShow="6" :slidesScroll="6" />
+    <app-head />
+    <app-about />
+    <app-touch-carousel
+      :isShow="showImage"
+      :slidesShow="showSlides"
+      :slidesScroll="scrollSlides"
+    />
     <section :class="[$style.catalogSection]">
       <div :class="[$style.catalogWrapper]">
         <div :class="[$style.iconHolder, $style.iconHolder_2]">
@@ -31,12 +16,13 @@
         <app-info-carousel :slidesShow="3" :slidesScroll="3" />
       </div>
     </section>
-    <section :class="[$style.layout]">
+    <section :class="[$style.feedback]">
       <div :class="[$style.layoutWrap]">
         <sharp-corner-image />
       </div>
       <div :class="[$style.layoutForm]">
         <icon-filled kind="fa-solid fa-file-pen" />
+        <app-submit-form />
       </div>
     </section>
     <!-- <feedback /> -->
@@ -46,11 +32,13 @@
 </template>
 
 <script>
-import AppHeader from "@/components/AppHeader.vue";
+import AppHead from "./index/partials/AppHead.vue";
+import AppAbout from "./index/partials/AppAbout.vue";
 import SectionTitle from "@/components/SectionTitle.vue";
 import IconFilled from "@/components/IconFilled.vue";
 import SharpCornerImage from "@/components/SharpCornerImage.vue";
-import AppCarousel from "@/components/AppCarousel.vue";
+import AppSubmitForm from "@/components/AppSubmitForm.vue";
+
 import AppTouchCarousel from "@/components/AppTouchCarousel.vue";
 import AppInfoCarousel from "@/components/AppInfoCarousel.vue";
 import AppFooter from "@/components/AppFooter.vue";
@@ -60,47 +48,30 @@ import LocationDisplay from "@/partials/LocationDisplaySection.vue";
 export default {
   name: "IndexContent",
   components: {
-    "app-header": AppHeader,
-    "app-carousel": AppCarousel,
+    "app-head": AppHead,
+    "app-about": AppAbout,
     "app-touch-carousel": AppTouchCarousel,
     "app-info-carousel": AppInfoCarousel,
     "section-title": SectionTitle,
     "icon-filled": IconFilled,
     "sharp-corner-image": SharpCornerImage,
+    "app-submit-form": AppSubmitForm,
 
     "location-display": LocationDisplay,
     "app-footer": AppFooter,
+  },
+  data: function () {
+    return {
+      showImage: true,
+      showSlides: 6,
+      scrollSlides: 6,
+    };
   },
 };
 </script>
 <style module>
 .app {
   height: 100%;
-}
-
-.backgroundImgContainer {
-  background-image: url("../../public/assets/Rostov.svg");
-  background-size: cover;
-}
-
-.carouselWrapper {
-  height: 100%;
-}
-
-.aboutSection {
-  background-color: #6630ff;
-  text-align: right;
-  padding: 1.3px 58.5px 39px 58.5px;
-}
-
-.aboutWrapper {
-  display: flex;
-}
-
-.aboutContent {
-  position: relative;
-  max-width: calc(50% - 9.75px);
-  margin-left: auto;
 }
 
 .iconHolder {
@@ -113,21 +84,6 @@ export default {
   padding: 0 26px;
 }
 
-.appAboutUsHeadline {
-  font-size: 16px;
-  line-height: 1.1;
-  text-transform: uppercase;
-  color: #fff;
-  padding: 260px 0 6.5px;
-}
-
-.appAboutUsSubline {
-  font-size: 35px;
-  line-height: 1.2;
-  color: #fff;
-  padding-bottom: 39px;
-}
-
 .catalogSection {
   background-color: #6630ff;
 }
@@ -137,32 +93,12 @@ export default {
   padding: 0 13px;
 }
 
+.feedback {
+  display: flex;
+  background-color: #6630ff;
+}
+
 @media all and (max-width: 360px) {
-  .appHeaderConatiner {
-    margin-bottom: 58.5px;
-  }
-
-  .aboutSection {
-    padding: 0;
-    margin-bottom: 58.5px;
-  }
-
-  .aboutContent {
-    padding: 0 19.5px;
-  }
-
-  .appAboutUsHeadline {
-    padding-left: 6.5px;
-    padding-right: 6.5px;
-    padding-bottom: 26px;
-  }
-
-  .appAboutUsSubline {
-    padding-left: 6.5px;
-    padding-right: 6.5px;
-    padding-bottom: 58.5px;
-  }
-
   .catalogSection {
     margin-top: 58.5px;
     padding: 0;
@@ -174,37 +110,17 @@ export default {
 }
 
 @media all and (max-width: 768px) {
-  .aboutSection {
-    position: relative;
-  }
-
-  .aboutContent {
-    max-width: 100%;
-    margin: 0;
-  }
-
   .iconHolder_2 {
+    padding: 0;
+  }
+
+  .feedback {
+    display: block;
     padding: 0;
   }
 }
 
 @media all and (min-width: 768px) {
-  .backgroundImgContainer {
-    margin-bottom: 130px;
-  }
-
-  .aboutSection {
-    margin-bottom: 130px;
-  }
-
-  .appAbouUsHeadline {
-    padding-bottom: 26px;
-  }
-
-  .appAboutUsSubline {
-    padding-bottom: 58.5px;
-  }
-
   .catalogSection {
     margin-top: 130px;
     padding-bottom: 65px;
@@ -212,17 +128,14 @@ export default {
 }
 
 @media all and (min-width: 1366px) {
-  .appHeaderConatiner {
-    margin-bottom: 58.5px;
-  }
-
-  .aboutSection {
-    margin-bottom: 58.5px;
-  }
-
   .catalogSection {
     margin-top: 58.5px;
     padding: 0;
+  }
+
+  .feedback {
+    padding-top: 58.5px;
+    margin-bottom: 58.5px;
   }
 }
 </style>
