8bd71d06bf1b9fce41175dc39b48f0669ee0f88b
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-08-13 01:41:18 +0300

    Добавлено частичное представление самой формы

---
+++ src/views/index/partials/AppFeedbackForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="[$style.feedbackForm]">
+    <icon-filled :kind="typeIcon" />
+    <app-submit-form />
+  </div>
+</template>
+
+<script>
+import IconFilled from "@/components/IconFilled.vue";
+import AppSubmitForm from "@/components/AppSubmitForm.vue";
+
+export default {
+  name: "AppFeedbackForm",
+  components: {
+    "icon-filled": IconFilled,
+    "app-submit-form": AppSubmitForm,
+  },
+  data: function () {
+    return {
+      typeIcon: "fa-solid fa-file-pen",
+    };
+  },
+};
+</script>
+
+<style module>
+.feedbackForm {
+  position: relative;
+  width: 60%;
+}
+
+@media all and (max-width: 768px) {
+  .feedbackForm {
+    padding: 260px 0 130px;
+  }
+}
+</style>
